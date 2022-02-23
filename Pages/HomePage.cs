04a47ebce6b334dcf8b5863faa703602cae57d38
AmjadIsmail <66602030+AmjadIsmail@users.noreply.github.com>
2022-02-23 11:27:08 +0000

    Add files via upload

---
+++ Pages/HomePage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumCoreDemo.Pages
+{
+   public class HomePage
+    {
+        private IWebDriver Driver { get; }
+        
+        public HomePage(IWebDriver webDriver)
+        {
+            Driver = webDriver;
+        }
+        public IWebElement lnkLogin => Driver.FindElement(By.LinkText("Login"));
+
+        public void ClickLogin() 
+        {
+            lnkLogin.Click();
+        
+        }
+
+        public IWebElement lnkEmployeeDetails => Driver.FindElement(By.LinkText("Employee Details"));
+
+        public bool IsEmployeeDetailsExists() => lnkEmployeeDetails.Displayed;
+
+        public void ClickEmployeeDetails() 
+        {
+            lnkEmployeeDetails.Click();
+        }
+    }
+}
